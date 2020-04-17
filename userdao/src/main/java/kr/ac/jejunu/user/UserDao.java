2249fc21abd77bb7f9359ab457e10d37234fddf5
Jimen <hurjeesic@naver.com>
2020-04-17 17:41:31 +0900

    포털서비스 개발론(3) - insert 추가 및 connection 리펙토링

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -5,11 +5,7 @@ import java.sql.*;
 public class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         // mysql
-        // driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // connection
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
-                "jeju", "jejupw");
+        Connection connection = getConnection();
         // query
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select id, name, password from userinfo where id = ?");
@@ -22,12 +18,40 @@ public class UserDao {
         user.setId(resultSet.getInt("id"));
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
-        // 자원해제
+        // 자원 해제
         resultSet.close();
         preparedStatement.close();
         connection.close();
 
         // 결과 return
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        // mysql
+        Connection connection = getConnection();
+        // query
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo(name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        // 실행
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        // 결과 매핑
+        user.setId(resultSet.getInt(1));
+        // 자원 해제
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        // driver 로딩
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // connection
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
+                "jeju", "jejupw");
     }
 }
