f91d1a4162c5f2b3a2f444bd27f13a7959ce4cb8
Jimen <hurjeesic@naver.com>
2020-04-17 17:54:17 +0900

    포털서비스 개발론(3) - Dao 추상화(Template Method Pattern, Factory Method Pattern)

---
+++ userdao/src/main/java/kr/ac/jejunu/user/JejuUserDao.java
@@ -0,0 +1,16 @@
+package kr.ac.jejunu.user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JejuUserDao extends UserDao {
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        // driver 로딩
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // connection
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
+                "jeju", "jejupw");
+    }
+}
--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         // mysql
         Connection connection = getConnection();
@@ -47,11 +47,10 @@ public class UserDao {
         connection.close();
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        // driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // connection
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
-                "jeju", "jejupw");
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+//        // driver 로딩
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        // connection
+//        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
+//                "jeju", "jejupw");
 }
