c849cee63255e13f83f2de01882b4c9870b962aa
Jimen <hurjeesic@naver.com>
2020-05-22 17:48:34 +0900

    포털서비스 개발론(8) - servlet을 이용한 spring 구동

---
+++ userdao/src/main/java/kr/ac/jejunu/user/UserServlet.java
@@ -0,0 +1,37 @@
+package kr.ac.jejunu.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UserServlet extends GenericServlet {
+    private UserDao userDao;
+
+    @Override
+    public void destroy() {
+        System.out.println("*********************** destroy ***********************");
+        super.destroy();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("*********************** init ***********************");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("kr.ac.jejunu.user");
+        this.userDao = applicationContext.getBean("userDao", UserDao.class);
+        super.init(config);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        User user = userDao.get(1);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html>");
+        stringBuilder.append("<h1>");
+        stringBuilder.append(String.format("Hello %s!!!", user.getName()));
+        stringBuilder.append("</h1>");
+        stringBuilder.append("</html>");
+        res.getWriter().println(stringBuilder.toString());
+    }
+}
