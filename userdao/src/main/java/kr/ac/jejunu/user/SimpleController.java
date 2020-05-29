9a6a6e8af939b9ee4ebce475277a8cbf01bcf396
Jimen <hurjeesic@naver.com>
2020-05-29 22:48:08 +0900

    포털서비스 개발론(9) - RequestMappingHandlerAdapter를 이용한 spring mvc 구현

--- userdao/src/main/java/kr/ac/jejunu/user/SimpleController.java
+++ userdao/src/main/java/kr/ac/jejunu/user/SimpleController.java
@@ -1,7 +1,6 @@
 package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -9,14 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
-public class SimpleController implements HttpRequestHandler {
+public class SimpleController implements Controller {
 	private final UserDao userDao;
 
 	@Override
-	public void handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		User user  = userDao.get(Integer.valueOf(request.getParameter("id")));
-		ModelAndView modelAndView = new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView("user");
 
 		modelAndView.addObject("user", user);
+
+		return modelAndView;
 	}
 }
