97dfc0f836e44235b1ae347f80608567d0f41b96
Jimen <hurjeesic@naver.com>
2020-05-29 22:32:02 +0900

    포털서비스 개발론(9) - 현재는 쓰이지 않는 SimpleServletHandlerAdapter 사용

--- userdao/src/main/java/kr/ac/jejunu/user/SimpleController.java
+++ userdao/src/main/java/kr/ac/jejunu/user/SimpleController.java
@@ -1,6 +1,7 @@
 package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -8,16 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
-public class SimpleController implements Controller {
+public class SimpleController implements HttpRequestHandler {
 	private final UserDao userDao;
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		User user  = userDao.get(Integer.valueOf(request.getParameter("id")));
 		ModelAndView modelAndView = new ModelAndView();
 
 		modelAndView.addObject("user", user);
-
-		return modelAndView;
 	}
 }
